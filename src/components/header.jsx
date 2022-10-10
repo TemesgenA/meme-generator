5ac0726baf0127ae1a1e71fb906d5bff5a42256d
TemesgeA <temesgen.ayneka@gmail.com>
2022-10-10 20:42:01 +0300

    Modify header component

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -6,7 +6,10 @@ function Header() {
         <header className="App-header">
             <div className="logo">
                 <img src={logo} className='logo-img' alt='logo' />
-                <p className="logo-title">Meme Generator</p>
+                <h2 className="logo-title">Meme Generator</h2>
+            </div>
+            <div className="nav-bar">
+                <p className="project-cat">Interactive React project</p>
             </div>
         </header>
     );
